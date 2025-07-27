95c25ff2cd8742b10295608583831e2cd4cda238
V. Akshaya <akshayavs2013@gmail.com>
2025-07-27 17:42:10 +0530

    Add files via upload

---
+++ Python Programs/skillrack - unique problems/calendar_month.py
@@ -0,0 +1,4 @@
+date=input()
+d,m,y=date.split("-")
+month_list=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+print(month_list[int(m)-1])
